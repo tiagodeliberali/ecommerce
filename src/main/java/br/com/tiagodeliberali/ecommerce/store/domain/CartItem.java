17db27bcd56e8ce5fb3815285955f860d6dfcead
tiagodeliberali <tiago.santos@quintoandar.com.br>
2021-05-07 09:29:25 -0300

    Add coart items

--- src/main/java/br/com/tiagodeliberali/ecommerce/store/domain/CartItem.java
+++ src/main/java/br/com/tiagodeliberali/ecommerce/store/domain/CartItem.java
@@ -14,4 +14,12 @@ public record CartItem(Product product, int quantity) {
     public Price getTotal() {
         return product.getValue().times(quantity);
     }
+
+    public CartItem add(CartItem cartItem) {
+        if (!this.product.equals(cartItem.product)) {
+            throw new IllegalArgumentException("Product of both items must be the same");
+        }
+
+        return new CartItem(product, quantity + cartItem.quantity());
+    }
 }
--- src/test/java/br/com/tiagodeliberali/ecommerce/store/domain/CartItemTests.java
+++ src/test/java/br/com/tiagodeliberali/ecommerce/store/domain/CartItemTests.java
@@ -41,4 +41,28 @@ public class CartItemTests {
 
         assertThat(total).isEqualTo(Price.ofDollar(45));
     }
+
+    @Test
+    public void add_two_cart_items_sums_its_quantity() {
+        Product product = new Product(new ProductId(UUID.randomUUID()), Price.ofDollar(15));
+        CartItem item1 = new CartItem(product, 3);
+        CartItem item2 = new CartItem(product, 5);
+
+        CartItem total = item1.add(item2);
+
+        assertThat(total.quantity()).isEqualTo(8);
+    }
+
+    @Test
+    public void add_two_cart_items_must_be_the_same_product() {
+        Product product1 = new Product(new ProductId(UUID.randomUUID()), Price.ofDollar(15));
+        CartItem item1 = new CartItem(product1, 3);
+
+        Product product2 = new Product(new ProductId(UUID.randomUUID()), Price.ofDollar(15));
+        CartItem item2 = new CartItem(product2, 5);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            item1.add(item2);
+        });
+    }
 }
